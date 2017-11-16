72d4ee4d19c786e1116ee18d9432d7afda51b885
Xingrang <xingrangl@bioconcepts.com.au>
2017-11-16 15:37:10 +1000

    Fizzbuzz class complested

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,10 +1,52 @@
 class FizzBuzz
 
    def initialize(value)
-       @value= value
+        @value=value
    end
 
+   def isFizz
+     if @value %3 ==0
+       return true
+     else
+       return false
+     end
+
+   end
+
+  def isBuzz
+     if @value % 5 ==0
+       return true
+     else
+       false
+     end
+  end
+
+
+  def printNumber
+    if isFizz and isBuzz
+      puts "FizzBuzz"
+    else
+      if isFizz
+        puts "Fizz"
+      else
+        if isBuzz
+          puts"Buzz"
+        else
+          puts @value
+        end
+      end
+
+    end
+
+  end
 
 end
 
-fizzTime=FizzBuzz.new(1)
+
+
+
+
+(1..100).each do |number|
+   fizzBuzz=FizzBuzz.new(number)
+   fizzBuzz.printNumber
+end
